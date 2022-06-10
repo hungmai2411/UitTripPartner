8f5bf47086007bc49a5ea587989d2b9b69f4bb3c
hungmai2411 <maiphamquochung@gmail.com>
2022-06-10 09:47:51 +0700

    add banner

---
+++ app/src/main/java/com/uittrippartner/hotel/Banner.java
@@ -0,0 +1,13 @@
+package com.uittrippartner.hotel;
+
+public class Banner {
+    String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
